939c32f4d268c1a13bccacadde3be8e24b3a138e
Mark Roosma <mark.roosma@gmail.com>
2022-04-06 11:42:25 -0700

    Initial Commit

---
+++ Criti/CritiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CritiApp.swift
+//  Criti
+//
+//  Created by Mark Roosma on 2022-04-06.
+//
+
+import SwiftUI
+
+@main
+struct CritiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
